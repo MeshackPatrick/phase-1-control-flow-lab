25c656ae71fc954dbdee69fd42b9e05fb4468f56
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-06-13 13:32:55 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride<=400){
+    return 'This one is on me!';
+  }
+  else if(ride > 2000 && ride <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else{
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  let trueStatement= 'Ok, sounds good.';
+  let falseStatement='No go.';
+  return city === 'NYC' ? trueStatement : falseStatement;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+ternaryCheckCity(`NYC`)
+ternaryCheckCity(`pittsburgh`)
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+  case `generous`:
+    return `Thank you so much.`;
+    break;
+    case `not as generous`:
+      return `Thank you.`
+      break;
+      default:
+        return`Bye.`
+      }
+}
+switchOnCharmFromTip(`generous`)
+switchOnCharmFromTip(`not generous`)
+switchOnCharmFromTip(`thanks for everything`)
